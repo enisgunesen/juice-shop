c8c98510606eed96a527e6d30ffc09d08edf3b89
enisgunesen <88477150+enisgunesen@users.noreply.github.com>
2025-12-05 06:12:38 -0500

    Update oauth.component.spec.ts

--- frontend/src/app/oauth/oauth.component.spec.ts
+++ frontend/src/app/oauth/oauth.component.spec.ts
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014-2025 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
+ * SPDX-License-Identifier: MIT  
  */
 
 import { type ComponentFixture, fakeAsync, TestBed, waitForAsync } from '@angular/core/testing'
